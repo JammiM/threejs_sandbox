0a86dc2199c7293d077db6c90997c89f5b232214
James Morrissey <jammim19@gmail.com>
2025-07-21 16:57:04 +0100

    Did testing with .glb files

--- 02-Vite/main.js
+++ 02-Vite/main.js
@@ -1,9 +1,9 @@
 import * as THREE from "three";
 
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 // const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader();
+const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
 
@@ -15,22 +15,69 @@ const far = 1000;
 
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+
+scene.background = new THREE.Color(0xffffff);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.append(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function addLights() {
+  const light = new THREE.DirectionalLight(0xffffff, 4);
+  light.position.set(30, -10, 20);
+  scene.add(light);
 
-camera.position.z = 5;
+  const ambientLight = new THREE.AmbientLight(0x404040); // soft light
+  scene.add(ambientLight);
+}
+
+function addModel(_url) {
+  // swith to loadAsync
+  loader.load(
+    _url,
+    function (gltf) {
+      let tempModel = new THREE.Object3D();
+
+      tempModel = gltf.scene;
+      tempModel.position.x = -50;
+
+      scene.add(tempModel);
+    },
+    (xhr) => {
+      console.log(`--: ${(xhr.loaded / xhr.total) * 100}% loaded`);
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
+}
+
+addModel("./models/low_poly_cloud.glb");
+addCube();
+addLights();
+
+function addCube() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.name = "doggy";
+  scene.add(cube);
+}
+
+// scene.getObjectsByProperty("type", "Mesh");
+// scene.getObjectByName();
+
+// scene.traverse((element) => {
+//   if (element instanceof THREE.Mesh) {
+//     console.log(element);
+//   }
+// });
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  scene.getObjectByName("doggy").position.z -= 0.01;
+  scene.getObjectById(5).position.x += 0.01;
+
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
